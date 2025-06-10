6391c378d1de1c6923e6522014655850c9d8378b
eduard villamil <villamilandrey02@gmail.com>
2025-06-09 23:36:35 -0500

    Se crean rutas estaticas para obtener la información del pokemon por id

--- src/utilities/getData.ts
+++ src/utilities/getData.ts
@@ -2,16 +2,17 @@ import type { FlavorTextEntry } from "../assets/interfaces/entry-flavor-text";
 import type { PokemonListResponse } from "../assets/interfaces/pokemon-list-response";
 import type { PokemonType } from "../assets/interfaces/pokemon-type";
 
-export const getPokemonData = async () => {
+
+export const getPokemonData = async (paramReturn:string) => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const { results } = await response.json() as PokemonListResponse;
 
   const pokemonData = await Promise.all(
     results.map(async ({ name, url }) => {
       // Obtener descripción
-      const requestDescriptionPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
-      const descripcionPokemon = await requestDescriptionPokemon.json();
-      const flavorTextEntries: FlavorTextEntry[] = descripcionPokemon.flavor_text_entries;
+      const pokemonDataRequest = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+      const pokemonDataResponse = await pokemonDataRequest.json();
+      const flavorTextEntries: FlavorTextEntry[] = pokemonDataResponse.flavor_text_entries;
       const entry = flavorTextEntries.find(entry => entry.language.name === "es");
 
       // Obtener tipos
@@ -19,13 +20,21 @@ export const getPokemonData = async () => {
       const typesPokemon = await requestTypes.json();
       const types: string[] = typesPokemon.types.map((type : PokemonType) => type.type.name);
 
+      const pokemonId = url.split("/").at(-2)
+      console.log( pokemonId )
+
+      const paramReturnValidator = paramReturn === "name"
+      const paramsReturn = paramReturnValidator ? { name : name} : {id: pokemonId}
+
+
       return {
-        params: { name },
+        params: paramsReturn ,
         props: { 
           name, 
           url, 
           descripcion: entry ? entry.flavor_text.replace(/\n/g, " ") : "Descripción no disponible",
-          types
+          types,
+          id : pokemonId
         } 
       };
     })
